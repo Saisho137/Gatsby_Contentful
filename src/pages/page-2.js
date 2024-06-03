caaf3a793e20e56996ddf5b36d9848d949b8509b
Santiago <santib.2001@hotmail.com>
2024-06-03 16:33:03 -0500

    test: Implemented basic Jest configuration for GatsbyJS with TS

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,10 +2,12 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import User from "../components/User/User"
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
+    <User name={'Santiago Betancur Duque'} bornDate={2002} currentDate={2024} />
   </Layout>
 )
 
